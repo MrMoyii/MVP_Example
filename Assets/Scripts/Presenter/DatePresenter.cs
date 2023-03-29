1cd65b22e122f213c4c1f5d8f1034027514cc91d
Omar Moyano <omarmoyano7w@gmail.com>
2023-03-29 14:02:32 -0300

    add scritps for presenter

---
+++ Assets/Scripts/Presenter/DatePresenter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatePresenter : MonoBehaviour, IView
+{
+    public void DisplayResult(string result)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string GetInputDate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
