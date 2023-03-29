cca5c967fadc2f65f4a2970c3e43715bb11cc68c
Omar Moyano <omarmoyano7w@gmail.com>
2023-03-29 14:52:29 -0300

    Update DatePresenter.cs

--- Assets/Scripts/Presenter/DatePresenter.cs
+++ Assets/Scripts/Presenter/DatePresenter.cs
@@ -1,21 +1,50 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 public class DatePresenter : IDatePresenter
 {
-    [SerializeField] private View view;
-    [SerializeField] private Date model;
-    public DatePresenter(View view)
+    [SerializeField] private IView view;
+    [SerializeField] private Date dateModel;
+
+    public DatePresenter(IView view)
     {
-        DateTime time= DateTime.Now;
+        string result;
         this.view = view;
-        model = new Date(time);
+        string dateInput = view.GetInputDate();
+        
+        DateTime converted_dateInput;
+        dateInput = dateInput.Trim(' ');
+
+        if (dateInput == String.Empty)
+        {
+            result = "Por favor, ingrese una fecha válida.";
+            Show(result);
+        }
+        else if (TryParseInput(dateInput, out converted_dateInput))
+        {   // date is correct
+            dateModel = new Date(converted_dateInput);
+            result = dateModel.DateIsPastPresentOrFuture();
+            Show(result);
+        }
+        else
+        {
+            result = "La fecha ingresada es inválida";
+            Show(result);
+        }
     }
 
-    public void Show()
+    private static bool TryParseInput(string dateInput, out DateTime converted_dateInput)
     {
-        throw new System.NotImplementedException();
+        return DateTime.TryParseExact(dateInput, "dd/MM/yyyy", CultureInfo.InvariantCulture,
+            DateTimeStyles.None, out converted_dateInput);
     }
+
+    public void Show(string result)
+    {
+        view.DisplayResult(result);
+    }
+
 }
