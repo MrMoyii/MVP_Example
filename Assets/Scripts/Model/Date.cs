154e11bbc5e73bf20428db9c4a6f6cbfbde4c55f
Omar Moyano <omarmoyano7w@gmail.com>
2023-03-29 14:26:42 -0300

    creation of the model script called date

---
+++ Assets/Scripts/Model/Date.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Date
+{
+    private DateTime dateInput;
+    private DateTime today = DateTime.UtcNow.Date;
+
+    public Date(DateTime dateInput)
+    {
+        this.dateInput = dateInput;
+    }
+
+    public string DateIsPastPresentOrFuture()
+    {
+        int result = DateTime.Compare(dateInput, today);
+        string stringResult = string.Empty;
+        if (result < 0)
+        {
+            stringResult = "La fecha ingresada es del PASADO.";
+        }
+        else if (result == 0)
+        {
+            stringResult = "La fecha ingresada es del PRESENTE.";
+        }
+        else if (result > 0)
+        {
+            stringResult = "La fecha ingresada es del FUTURO.";
+        }
+
+        return stringResult;
+    }
+}
