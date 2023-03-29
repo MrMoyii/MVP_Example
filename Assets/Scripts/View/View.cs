2b42a8b1d7ff33931d998804ccf95693cfba08bb
Omar Moyano <omarmoyano7w@gmail.com>
2023-03-29 14:18:29 -0300

    connect view with presenter

--- Assets/Scripts/View/View.cs
+++ Assets/Scripts/View/View.cs
@@ -2,15 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class View : MonoBehaviour, IDatePresenter
+public class View : MonoBehaviour, IView
 {
+    [SerializeField] private Text result;
+    [SerializeField] private IDatePresenter presenter;
     private void Start()
     {
-        
+        presenter = new DatePresenter(this);
     }
 
-    public void Show()
+    public void DisplayResult(string result)
     {
         throw new NotImplementedException();
     }
