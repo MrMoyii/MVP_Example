1eef9074530979749dd6e4a457f79fc2bee1569d
Omar Moyano <omarmoyano7w@gmail.com>
2023-03-29 14:00:28 -0300

    add scripts for view

---
+++ Assets/Scripts/View/IView.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public interface IView
+{
+    void DisplayResult(string result);
+    string GetInputDate();
+}
---
+++ Assets/Scripts/View/View.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View : MonoBehaviour, IView
+{
+    private void Start()
+    {
+        
+    }
+    public void DisplayResult(string result)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetInputDate()
+    {
+        throw new NotImplementedException();
+    }
+}
